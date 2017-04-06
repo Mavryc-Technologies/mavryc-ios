50e87015b2b65740fabd292b0562f5133037b89a
Todd Hopkinson <toddhopkinson@gmail.com>
2017-04-06 10:55:15 -0700

    adds realm pod

---
+++ mavryc/mavryc/Cities.swift
@@ -0,0 +1,20 @@
+//
+//  Cities.swift
+//  mavryc
+//
+//  Created by Todd Hopkinson on 4/6/17.
+//  Copyright © 2017 Mavryc Technologies, Inc. All rights reserved.
+//
+
+import Foundation
+
+class Cities {
+    
+    // call site
+    /*
+        vc.datasource = Cities.autocompleteSource
+        vc.datasource.matches("ph") // returns ["Phoenix", "Philadelphia", "Pharmichael"]
+     */
+    
+    
+}
