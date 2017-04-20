322f56d90b89e8681299186bc7b2cbbdf1d5ef08
Todd Hopkinson <toddhopkinson@gmail.com>
2017-04-20 10:36:17 -0700

    adds call to signup/ mock server

--- mavryc/mavryc/SignupClient.swift
+++ mavryc/mavryc/SignupClient.swift
@@ -22,8 +22,9 @@ class SignupClient {
                                      birthday: signup.birthday)
         let url = Networking.url(route: api)
         let params = Networking.payload(source: api)
+        let method = Networking.httpMethod(source: api)
         
-        Networking.sharedInstance.sessionManager.request(url, method: .post, parameters: params, encoding: URLEncoding.default, headers: nil).validate().responseJSON { response in
+        Networking.sharedInstance.sessionManager.request(url, method: method, parameters: params, encoding: URLEncoding.default, headers: nil).validate().responseJSON { response in
             
             switch response.result {
             case .success(let jsonData):
@@ -31,29 +32,7 @@ class SignupClient {
                 let json:JSON = JSON(jsonData)
                 SafeLog.print("response \(json)")
                 
-                //                do {
-                //                    try json.validatePlatformResponseSuccess()
-                //                } catch  {
-                //                    failure?(error)
-                //                    print("📡 response: \(response)")
-                //                    return
-                //                }
-                
-                // Parse the user object
-                //                do {
-                //                    if let user = try User.parseServerData(json: json) {
-                //                        success?(user)
                 success?(json.debugDescription)
-                //                        return
-                //                    }
-                //                } catch {
-                //                    print("caught thrown parsing error")
-                //                    failure?(error)
-                //                    print("📡 response: \(response)")
-                //                    return
-                //                }
-                
-                //                failure?(NSError(domain: "httpresponsesuccessful.butnotreally.somethingiswrong", code: 999, userInfo: nil))
                 
             case .failure(let error):
                 SafeLog.print(error)
--- mavryc/mavrycTests/mavrycTests.swift
+++ mavryc/mavrycTests/mavrycTests.swift
@@ -21,14 +21,7 @@ class mavrycTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
     func testCitySearchAutoCompletionSuggestions() {
         let exp = expectation(description: "test autocompletion")
         let citySource = Cities.shared
