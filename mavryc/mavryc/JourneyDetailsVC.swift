5194864682925cf7973236930083af147d8b535d
Todd Hopkinson <toddhopkinson@gmail.com>
2017-05-12 14:10:07 -0700

    adds universal pdf icon for form (departure icon) updates panel view colors adds initial control arranges spacing of elements on panel view

---
+++ mavryc/mavryc/JourneyDetailsVC.swift
@@ -0,0 +1,35 @@
+//
+//  JourneyDetailsVC.swift
+//  mavryc
+//
+//  Created by Todd Hopkinson on 5/12/17.
+//  Copyright © 2017 Mavryc Technologies, Inc. All rights reserved.
+//
+
+import UIKit
+
+class JourneyDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
