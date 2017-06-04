0151c1dc98184008df321c7e8eeaa4f1740f5891
Todd Hopkinson <toddhopkinson@gmail.com>
2017-06-04 00:05:40 -0700

    adds stubbed ServicesTableViewController hooks next to go show ServicesTableViewController

--- mavryc/mavryc/AirportSearchTableViewController.swift
+++ mavryc/mavryc/AirportSearchTableViewController.swift
@@ -14,11 +14,6 @@ class AirportSearchTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
